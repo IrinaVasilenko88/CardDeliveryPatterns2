e92704ae9987282c256a64edc5946e4a06ed676f
Irina Vasilenko <vasilenko_i@yahoo.com>
2020-12-16 18:01:26 +0300

    test added

--- src/test/java/ru/netology/test/LogInTest.java
+++ src/test/java/ru/netology/test/LogInTest.java
@@ -6,22 +6,25 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Locale;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static ru.netology.data.DataGenerator.generateValidUser;
 
 public class LogInTest {
+
     @BeforeEach
     void setUp(){
-        open ("http://localhost:9999/";
+        open ("http://localhost:9999/");
     }
+
     @Test
     void shouldLogIn(){
     val RequestData = generateValidUser();
     $ ("[data-test-id=login][name=login]").setValue(RequestData.getLogin());
     $ ("[data-test-id=password][name=password]").setValue(RequestData.getPassword());
     $ ("[data-test-id=action-login][class=button__text]").click();
-
+    $("h2").shouldHave(text("Личный кабинет"));
 
     }
 }
