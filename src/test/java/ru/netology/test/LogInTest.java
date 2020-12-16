f9bbc7dd148766993654c48a485ff27e3cb918b7
Irina Vasilenko <vasilenko_i@yahoo.com>
2020-12-16 21:32:20 +0300

    tests without result added

--- src/test/java/ru/netology/test/LogInTest.java
+++ src/test/java/ru/netology/test/LogInTest.java
@@ -9,7 +9,7 @@ import java.util.Locale;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static ru.netology.data.DataGenerator.generateValidUser;
+import static ru.netology.data.DataGenerator.*;
 
 public class LogInTest {
 
@@ -25,6 +25,38 @@ public class LogInTest {
     $ ("[data-test-id=password][name=password]").setValue(RequestData.getPassword());
     $ ("[data-test-id=action-login][class=button__text]").click();
     $("h2").shouldHave(text("Личный кабинет"));
+    }
+    @Test
+    void shouldShowUserAsBlocked(){
+    val RequestData = generateBlockedUser();
+    $ ("[data-test-id=login][name=login]").setValue(RequestData.getLogin());
+    $ ("[data-test-id=password][name=password]").setValue(RequestData.getPassword());
+    $ ("[data-test-id=action-login][class=button__text]").click();
 
     }
+    @Test
+    void shouldShowInvalidPassword(){
+    val RequestData = generateUserWithInvalidPassword();
+    $ ("[data-test-id=login][name=login]").setValue(RequestData.getLogin());
+    $ ("[data-test-id=password][name=password]").setValue(RequestData.getPassword());
+    $ ("[data-test-id=action-login][class=button__text]").click();
+
+    }
+    @Test
+    void shouldShowInvalidLogin(){
+    val RequestData = generateUserWithInvalidLogin();
+    $ ("[data-test-id=login][name=login]").setValue(RequestData.getLogin());
+    $ ("[data-test-id=password][name=password]").setValue(RequestData.getPassword());
+    $ ("[data-test-id=action-login][class=button__text]").click();
+
+    }
+    @Test
+    void shouldShowNonRegisteredUser(){
+    val RequestData = generateNonRegisteredUser();
+    $ ("[data-test-id=login][name=login]").setValue(RequestData.getLogin());
+    $ ("[data-test-id=password][name=password]").setValue(RequestData.getPassword());
+    $ ("[data-test-id=action-login][class=button__text]").click();
+
+    }
+
 }
